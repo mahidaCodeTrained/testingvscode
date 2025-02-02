4d8f1eae9936ffa7dc3ee1d97504b4728a18d98d
mahidaCodeTrained <abhishekmahida56@gmail.com>
2025-02-02 10:02:31 +0000

    add placeholder python code to test venv works.

---
+++ run.py
@@ -0,0 +1,25 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+num = int(input("Enter a number: "))
+
+
+if num > 0:
+    sign = "positive"
+elif num < 0:
+    sign = "negative"
+else:
+    sign = "zero"
+
+if is_prime(num):
+    prime_status = "a prime number"
+else:
+    prime_status = "not a prime number"
+
+print(f"The number is {sign} and {prime_status}.")
