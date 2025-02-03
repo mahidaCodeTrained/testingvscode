47e54e8d1bdce9f9216c560a47ff4e12423acaf8
mahidaCodeTrained <abhishekmahida56@gmail.com>
2025-02-03 12:38:41 +0000

    testing python code

--- run.py
+++ run.py
@@ -1,26 +1,21 @@
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+class AnimalList():
+
+    """
+    This will create an animal list
+    """
+
+    def __init__(self, name, description, color):
+        self.name = name
+        self.description = description
+        self.color = color
+
+    def get_overview(self):
+        return f"{self.name} and {self.description}"
+
+    def full_info(self):
+        return f"{self.name} and {self.description} and {self.color}"
 
 
-num = int(input("Enter a number: "))
-
-
-if num > 0:
-    sign = "positive"
-elif num < 0:
-    sign = "negative"
-else:
-    sign = "zero"
-
-if is_prime(num):
-    prime_status = "a prime number"
-else:
-    prime_status = "not a prime number"
-
-print(f"The number is {sign} and {prime_status}.")
-5
+list = AnimalList('Jaguar', 'It is so fast its very crazy', 'Blue')
+print(list.color)
+print(list.get_overview())
