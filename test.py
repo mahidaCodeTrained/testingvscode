3b98f9de309f1def82e47d663dd388c45d19e8af
Shivani Mahida <abhishekmahida56@gmail.com>
2025-02-10 15:37:09 +0000

    test

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 user_name = input("What is your username: ")
-print()
 
 if user_name:
     print(f"Thank you we got it {user_name}")
+else:
+    print("You need to enter a username")
+
+user_age = input("How old are you: ")
+
+if user_age is int:
+    print(f"Thanks so much you are {user_age}")
+else:
+    print("This is not a valid age.")
